bb033d6c00dbe351952761060a77e48a47d90d5f
codervaruns <varunshankar.shan@gmail.com>
2024-12-18 21:20:58 +0530

    made some changes for testing server configuration

--- carpooling-backend/config/db.config.js
+++ carpooling-backend/config/db.config.js
@@ -7,8 +7,21 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
     dialect: 'mysql',
-    logging: false
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
   }
 );
 
